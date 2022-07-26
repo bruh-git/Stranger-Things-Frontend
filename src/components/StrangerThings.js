c8964425b7d144bfb8b9aaddf8ee09b9e399ca23
bruh-git <brunac.cardososilva@gmail.com>
2022-07-26 18:52:40 -0300

    Fix eslint

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -25,7 +25,6 @@ const upsideDownConfig = {
   timeout: REACT_APP_UPSIDEDOWN_TIMEOUT,
 };
 
-
 const charactersService = new CharactersService(strangerThingsConfig);
 const charactersUpsideDownService = new CharactersService(upsideDownConfig);
 
